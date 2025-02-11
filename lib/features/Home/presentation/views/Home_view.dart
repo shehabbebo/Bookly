d675aeb2bbe4fe9b54e6188fa39f73e81786e68e
shehabbebo <shehabbebo272gmail.com>
2025-02-11 15:09:53 +0200

    add custom appbar to home

--- lib/features/Home/presentation/views/Home_view.dart
+++ lib/features/Home/presentation/views/Home_view.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/features/Home/presentation/views/weight/homeview_body.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -5,6 +6,8 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: HomeviewBody(),
+    );
   }
 }
---
+++ lib/features/Home/presentation/views/weight/homeview_body.dart
@@ -0,0 +1,17 @@
+import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/features/Home/presentation/views/weight/CustomAppBar.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomeviewBody extends StatelessWidget {
+  const HomeviewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CustomAppBar(),
+      ],
+    );
+  }
+}
