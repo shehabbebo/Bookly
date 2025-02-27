4d11d362b3cb515eb44366b2afad64f38c816231
shehabbebo <shehabbebo272gmail.com>
2025-02-27 20:27:31 +0200

    best seller done

---
+++ lib/features/Home/presentation/views/weight/best_seller_list_view.dart
@@ -0,0 +1,20 @@
+import 'package:bookly/features/Home/presentation/views/weight/best_Seller_itemView.dart';
+import 'package:flutter/material.dart';
+
+class BestSellerlistView extends StatelessWidget {
+  const BestSellerlistView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        physics: NeverScrollableScrollPhysics(),
+        padding: EdgeInsets.zero,
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: BestSellerListViewItem(),
+          );
+        });
+  }
+}
--- lib/features/Home/presentation/views/weight/homeview_body.dart
+++ lib/features/Home/presentation/views/weight/homeview_body.dart
@@ -3,33 +3,47 @@ import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/Home/presentation/views/weight/CustomAppBar.dart';
 import 'package:bookly/features/Home/presentation/views/weight/best_Seller_itemView.dart';
+import 'package:bookly/features/Home/presentation/views/weight/best_seller_list_view.dart';
 import 'package:bookly/features/Home/presentation/views/weight/featurelistView.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeviewBody extends StatelessWidget {
   const HomeviewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomAppBar(),
-          FeatureBooksListView(),
-          SizedBox(
-            height: 50,
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: CustomAppBar(),
+              ),
+              FeatureBooksListView(),
+              SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Text(
+                  'Best Seller',
+                  style: styles.TextStyle18,
+                ),
+              ),
+              SizedBox(height: 20),
+            ],
           ),
-          Text(
-            'Best Seller',
-            style: styles.TextStyle18,
+        ),
+        SliverFillRemaining(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: BestSellerlistView(),
           ),
-          SizedBox(height: 20),
-          BestSellerListViewItem(),
-        ],
-      ),
+        )
+      ],
     );
   }
 }
